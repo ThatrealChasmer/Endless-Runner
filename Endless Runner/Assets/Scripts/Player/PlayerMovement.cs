3032087eaed0483da297c4d9cffd839f9dd9c937
Chasmer <36886220+ThatrealChasmer@users.noreply.github.com>
2018-11-27 12:53:09 +0100

    Switched jumping to Space

--- Endless Runner/Assets/Scripts/Player/PlayerMovement.cs
+++ Endless Runner/Assets/Scripts/Player/PlayerMovement.cs
@@ -9,8 +9,6 @@ public class PlayerMovement : MonoBehaviour {
     private bool isGrounded;
     public int maxJumps;
     private int jumpsLeft;
-    public int maxJumpTime;
-    private int jumpTime = 0;
     public GameObject gm;
     private bool jump;
     public float maxFallMultiplier;
@@ -25,21 +23,21 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
+        //if (Input.touchCount > 0)
+        //{
+           // Touch touch = Input.GetTouch(0);
 
-            if(touch.phase == TouchPhase.Began && jumpsLeft > 0)
+        if (Input.GetButtonDown("Jump") && jumpsLeft > 0)
             {
                 rb.AddForce(Vector2.up * jumpForce,ForceMode2D.Impulse);
                 jumpsLeft -= 1;
             }
-            if(touch.phase == TouchPhase.Ended && rb.velocity.y > 0)
+            if(Input.GetButtonUp("Jump") && rb.velocity.y > 0)
             {
                 rb.AddForce(Vector2.down * rb.velocity.y / 2, ForceMode2D.Impulse);
             }
             rb.gravityScale = 1 - rb.velocity.y * maxFallMultiplier;
-        }
+        //}
 	}
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -48,7 +46,6 @@ public class PlayerMovement : MonoBehaviour {
         {
             jumpsLeft = maxJumps;
             isGrounded = true;
-            jumpTime = 0;
         }
 
         if (collision.collider.gameObject.transform.CompareTag("Wall"))
